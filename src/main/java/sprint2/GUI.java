01d751bafca08e37c297b104154dceb1c2bf3e94
KevMonte <kevmontes00@gmial.com>
2025-10-25 19:54:32 -0500

    Mostly finished GUI stuff

--- src/main/java/sprint2/GUI.java
+++ src/main/java/sprint2/GUI.java
@@ -9,35 +9,139 @@ public class GUI extends JFrame {
 	//JFrame frame = new JFrame("SOS game");
 	public JButton button2;
 	public JButton button1;
+	SidePanel leftPanel;
+	SidePanel rightPanel;
+	BoardPanel boardPanel;
+	JPanel boardSizeLimiter;
+	TopPanel topPanel;
 	public GUI() {
 	
 	
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
-	JButton button2= new JButton("Hello");
-	JButton button1 = new JButton("Hello 2");
 	
 	JPanel mainPanel = new JPanel();
 	
-	JPanel leftPanel = new JPanel();
 	
-	BoxLayout boxLayoutManager = new BoxLayout(leftPanel, BoxLayout.Y_AXIS);
+	
+	
 	
 	BorderLayout borderLayoutManager = new BorderLayout();
+
 	setTitle("SOS Game");
-	setSize(400, 300);
+	setSize(800, 600);
 	mainPanel.setLayout(borderLayoutManager);
-	leftPanel.setLayout(boxLayoutManager);
+	leftPanel = new SidePanel("Red PLayer");
+	rightPanel = new SidePanel("Blue PLayer");
+	boardPanel= new BoardPanel(20);
+	boardSizeLimiter = new JPanel(new FlowLayout(FlowLayout.CENTER));
+	topPanel = new TopPanel();
 	
-	leftPanel.add(Box.createVerticalGlue());
-	
-	leftPanel.add(button1);
-	leftPanel.add(button2);
-	leftPanel.add(Box.createVerticalGlue());
-	
+
+	boardPanel.setPreferredSize(new Dimension(500,500));
+
+	boardSizeLimiter.add(boardPanel);
+	boardSizeLimiter.add(Box.createVerticalGlue());
+
+	add(Box.createRigidArea(new Dimension(25, 0)),borderLayoutManager.WEST);
+	add(Box.createRigidArea(new Dimension(25, 0)),borderLayoutManager.EAST);
+	//TO DO: make it so when right panel has an option clicked the other panel toggles, for example of left panel has S clicked, right panel will have O clicked.
 	mainPanel.add(leftPanel,borderLayoutManager.WEST);
+	leftPanel.sButton.setSelected(true);
+	mainPanel.add(rightPanel,borderLayoutManager.EAST);
+	rightPanel.oButton.setSelected(true);
+	//mainPanel.add(boardPanel);
+	mainPanel.add(boardSizeLimiter);
+	mainPanel.add(topPanel,borderLayoutManager.NORTH);
+	
 	add(mainPanel);
 	
 	
+	
+	
+	
+	
+	}
+
+}
+
+ class SidePanel extends JPanel {
+	JLabel playerLabel;
+	JRadioButton sButton;
+	JRadioButton oButton;
+	ButtonGroup soButtonGroup;
+	public SidePanel(String playerColor) {
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		add(Box.createVerticalGlue());
+
+		playerLabel = new JLabel(playerColor);	
+		add(playerLabel);
+		add(Box.createRigidArea(new Dimension(0, 10)));
+
+		sButton = new JRadioButton("S");
+		oButton = new JRadioButton("O");
+		soButtonGroup = new ButtonGroup();
+
+		soButtonGroup.add(sButton);
+		soButtonGroup.add(oButton);
+
+		add(sButton);
+		add(oButton);
+
+
+		add(Box.createVerticalGlue());
+	}
+}
+class BoardPanel extends JPanel {
+	JButton[][] boardButtons;
+
+	public BoardPanel(int boardSize){
+		setLayout(new GridLayout(boardSize,boardSize));
+		boardButtons = new JButton[boardSize][boardSize];
+
+		//setSize(new Dimension(200,200));
+		setPreferredSize(new Dimension(100,100));
+		for (int row = 0; row < boardSize; row++) {
+			for (int col = 0; col < boardSize; col++) {
+				JButton cell= new JButton("");
+				boardButtons[row][col]= cell;
+				add(cell);
+
+			}
+		}
+	}
+}
+
+class TopPanel extends JPanel{
+	JLabel sosLabel;
+	JLabel sizeLabel;
+	JRadioButton simpleButton;
+	JRadioButton generalButton;
+	ButtonGroup gamemodeButtonGroup;
+	JTextField boardSizeField;
+	public TopPanel(){
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		sosLabel= new JLabel("SOS Game Mode:");
+		sizeLabel= new JLabel("Board Size");
+		simpleButton= new JRadioButton("Simple",true);
+		generalButton= new JRadioButton("General");
+		boardSizeField = new JTextField("3",3);
+		
+
+		gamemodeButtonGroup = new ButtonGroup();
+
+		gamemodeButtonGroup.add(simpleButton);
+		gamemodeButtonGroup.add(generalButton);
+
+
+
+		add(sosLabel);
+		add(simpleButton);
+		add(generalButton);
+		add(Box.createRigidArea(new Dimension(100, 10)));
+		add(sizeLabel);
+		add(boardSizeField);
+
+
 	}
 }
